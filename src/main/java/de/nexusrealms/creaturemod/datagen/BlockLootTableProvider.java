aee1abc3c57d7d79287f07b79b6109f3fe7be037
farpo <77583105+farpo@users.noreply.github.com>
2025-01-29 22:06:48 +0100

    look at this garlic clove

--- src/main/java/de/nexusrealms/creaturemod/datagen/BlockLootTableProvider.java
+++ src/main/java/de/nexusrealms/creaturemod/datagen/BlockLootTableProvider.java
@@ -1,8 +1,12 @@
 package de.nexusrealms.creaturemod.datagen;
 
 import de.nexusrealms.creaturemod.blocks.ModBlocks;
+import de.nexusrealms.creaturemod.items.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.AllOfLootCondition;
+import net.minecraft.loot.condition.LootCondition;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
@@ -14,6 +18,7 @@ public class BlockLootTableProvider extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
+        //TODO Add actual drops
         addDrop(ModBlocks.GARLIC);
     }
 }
--- src/main/java/de/nexusrealms/creaturemod/items/ModItems.java
+++ src/main/java/de/nexusrealms/creaturemod/items/ModItems.java
@@ -1,15 +1,13 @@
 package de.nexusrealms.creaturemod.items;
 
 import de.nexusrealms.creaturemod.CreatureMod;
+import de.nexusrealms.creaturemod.blocks.ModBlocks;
 import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.component.type.FoodComponents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -17,7 +15,7 @@ import net.minecraft.registry.RegistryKey;
 import java.util.function.BiConsumer;
 
 public class ModItems {
-    public static final Item GARLIC_CLOVE = create("garlic_clove", new Item(new Item.Settings().food(FoodComponents.GARLIC_CLOVE)), ItemGroups.FOOD_AND_DRINK,
+    public static final Item GARLIC_CLOVE = create("garlic_clove", new AliasedBlockItem(ModBlocks.GARLIC, new Item.Settings().food(FoodComponents.GARLIC_CLOVE)), ItemGroups.FOOD_AND_DRINK,
             (fabricItemGroupEntries, item) -> fabricItemGroupEntries.addAfter(Items.BEETROOT, item));
     public static final Item GARLIC_BULB = create("garlic_bulb", new Item(new Item.Settings()), ItemGroups.NATURAL);
     private static <T extends Item> T create(String name, T item, RegistryKey<ItemGroup> group){
