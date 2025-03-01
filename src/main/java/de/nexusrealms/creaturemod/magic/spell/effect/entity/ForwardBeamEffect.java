48474306a5fcc4bab0bfb6bba643d2eec8e2e3bc
farpo <77583105+farpo@users.noreply.github.com>
2025-03-01 19:21:44 +0100

    Particle refactoring in preparation for spell VFX

--- src/main/java/de/nexusrealms/creaturemod/magic/spell/effect/entity/ForwardBeamEffect.java
+++ src/main/java/de/nexusrealms/creaturemod/magic/spell/effect/entity/ForwardBeamEffect.java
@@ -5,13 +5,26 @@ import com.mojang.serialization.MapCodec;
 import de.nexusrealms.creaturemod.CreatureMod;
 import de.nexusrealms.creaturemod.magic.spell.effect.SpellEffect;
 import de.nexusrealms.creaturemod.magic.spell.effect.SpellEffectType;
+import foundry.veil.api.client.render.VeilRenderSystem;
+import foundry.veil.api.quasar.data.ParticleSettings;
+import foundry.veil.api.quasar.data.QuasarParticleData;
+import foundry.veil.api.quasar.particle.ParticleEmitter;
+import foundry.veil.api.quasar.particle.ParticleSystemManager;
+import foundry.veil.api.resource.VeilDynamicRegistry;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleUtil;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
-public record ForwardBeamEffect(int color) implements SpellEffect<Entity> {
-    public static final MapCodec<ForwardBeamEffect> CODEC = Codec.INT.fieldOf("color").xmap(ForwardBeamEffect::new, ForwardBeamEffect::color);
+public record ForwardBeamEffect(RegistryEntry<QuasarParticleData> particleSettings) implements SpellEffect<Entity> {
+    public static final MapCodec<ForwardBeamEffect> CODEC = QuasarParticleData.CODEC.fieldOf("particleSettings").xmap(ForwardBeamEffect::new, ForwardBeamEffect::particleSettings);
+    public static final Identifier BEAM_EMITTER = CreatureMod.id("colored_beam_emitter");
     @Override
     public SpellEffectType<? extends SpellEffect<Entity>> getType() {
         return SpellEffectType.FORWARD_BEAM;
@@ -19,7 +32,12 @@ public record ForwardBeamEffect(int color) implements SpellEffect<Entity> {
 
     @Override
     public boolean apply(PlayerEntity origin, Entity target, @Nullable ItemStack castingItem, @Nullable Entity clickTarget) {
-        CreatureMod.LOGGER.info("Yea yea this logs on the client too");
+        if(origin.getWorld() instanceof ClientWorld clientWorld){
+            //TODO Figure why Veil particles are weirdly coupled and what that means for my plans
+/*            ParticleSystemManager manager = VeilRenderSystem.renderer().getParticleManager();
+            ParticleEmitter emitter = manager.createEmitter(BEAM_EMITTER);
+            emitter.setParticleData(particleSettings.value());*/
+        }
         return true;
     }
 }
